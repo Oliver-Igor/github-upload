c59ee3cd4acc312a45fb85391038aeb7e2637667
Oliver-Igor <valterigor.florestal@gmail.com>
2022-03-22 23:01:04 -0300

    initial commit - moving project

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'DEPUTADO',
+        numero: 5,
+        candidato: [
+            {
+                numero:'13456',
+                nome: 'Geralt de Rivia',
+                partido: 'PTdeRIVIA',
+                fotos:[
+                    {url: 'geralt.png', legenda: 'Deputado Federal'}
+                ]
+            },
+            {
+                numero:'50000',
+                nome: 'Jaskier',
+                partido: 'PSOLdeRIVIA',
+                fotos:[
+                    {url: 'jaskier.png', legenda: 'Deputado Federal'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numero: 2,
+        candidato: [
+            {
+                numero:'13',
+                nome: 'Shrek',
+                partido: 'PT',
+                vice: 'Burro',
+                fotos:[
+                    {url: 'Shrek.png', legenda: 'Presidente'},
+                    {url: 'burro.png', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero:'50',
+                nome: 'Klaus',
+                partido: 'ABC',
+                vice: 'Caramelo',
+                fotos:[
+                    {url: 'igor.png', legenda: 'Presidente'},
+                    {url: 'caramelo.png', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+        ]
+    },
+]
